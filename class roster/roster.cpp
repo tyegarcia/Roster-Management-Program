6578f1bb0d3b6411b4cd99d030f67526a322f9dd
Tye <79384529+tyegarcia@users.noreply.github.com>
2022-03-17 16:07:19 -0700

    Update roster.cpp

--- class roster/roster.cpp
+++ class roster/roster.cpp
@@ -6,7 +6,7 @@ Roster::Roster(int maxSize) {
 		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK"	,
 		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE"		,
 		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY"		,
-		"A5,Tye,Garcia,tgar187@wgu.edu,31,15,31,45,SOFTWARE"
+		"A5,Tye,Garcia,tye@wgu.edu,31,15,31,45,SOFTWARE"
 	};
 
 	this->lastIndex = -1;
